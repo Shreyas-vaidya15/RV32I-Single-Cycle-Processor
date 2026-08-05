9dae2ce76094b17107d3b0f70b88e942694e0326
Shreyas Vaidya <vaidyashreyas89@gmail.com>
2026-08-06 01:39:03 +0530

    Added pipeline register modules, no integration yet

---
+++ IF_ID_reg.v
@@ -0,0 +1,27 @@
+module IF_ID_reg
+(
+    input clk, reset,
+    input [31:0] Instr_In, PC_In, PC_Plus_4_In,
+    output reg [31:0] Instr_Out, PC_Out, PC_Plus_4_Out
+);
+
+always@(posedge clk or posedge reset)
+begin
+
+if(reset)
+begin
+Instr_Out <= 32'b0;
+PC_Out <= 32'b0;
+PC_Plus_4_Out <= 32'b0;
+end
+
+else
+begin
+Instr_Out <= Instr_In;
+PC_Out <= PC_In;
+PC_Plus_4_Out <= PC_Plus_4_In;
+end
+
+end
+
+endmodule
